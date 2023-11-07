e6adb9d2ccd891104522c7bca7ac2db9d4f4b05e
rajan <36818845+hirajanwin@users.noreply.github.com>
2023-11-07 14:21:50 +0000

    Update 3 files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,51 @@
-import React from 'react'
-import Link from "next/link"
-import AdditionalLinksForm from '@/components/forms/AdditionalLinksForm'
-import ProfileForm from '@/components/forms/ProfileForm'
-import SocialLinksForm from '@/components/forms/SocialLinksForm'
-import MobileMockup from '@/components/MobileMockup'
-import PreviewButton from '@/components/buttons/PreviewButton'
-import DemoData from '@/components/buttons/DemoData'
-import { buttonVariants } from '@/components/ui/button'
-import Publish from '@/components/buttons/Publish'
-import { Github } from 'lucide-react'
-import ShortenerButton from '@/components/buttons/ShortenerButton'
-import Background from '@/components/background/Background'
+import React from "react";
+import Link from "next/link";
+import AdditionalLinksForm from "@/components/forms/AdditionalLinksForm";
+import ProfileForm from "@/components/forms/ProfileForm";
+import SocialLinksForm from "@/components/forms/SocialLinksForm";
+import MobileMockup from "@/components/MobileMockup";
+import PreviewButton from "@/components/buttons/PreviewButton";
+import DemoData from "@/components/buttons/DemoData";
+import { buttonVariants } from "@/components/ui/button";
+import Publish from "@/components/buttons/Publish";
+import { Github } from "lucide-react";
+import ShortenerButton from "@/components/buttons/ShortenerButton";
+import Background from "@/components/background/Background";
 
 export const siteConfig = {
-  name: "LinkNode - one page, many links.",
-  description: "LinkNode is an open-source tool that helps you generate one link for all your links.",
-  ogImage: "https://linknode.vercel.app/og-image.png",
-  url: "https://linknode.vercel.app",
-}
+  name: "Bio.New - Free Link in Bio to curate many links in one page.",
+  description:
+    "LinkNode is an open-source tool that helps you generate one link for all your links.",
+  ogImage: "https://bio.new/og-image.png",
+  url: "https://bio.new",
+};
 
 export default function Home() {
   return (
-    <main className='relative grid lg:grid-cols-3 h-screen px-2 lg:px-0 md:container'>
-      <section className='lg:col-span-2 flex flex-col items-center justify-center py-6 lg:px-20 gap-6 h-screen'>
-        <div className='overflow-y-auto w-full hide_scrollbar flex flex-col gap-5 pb-20 lg:pb-0'>
-
+    <main className="relative grid lg:grid-cols-3 h-screen px-2 lg:px-0 md:container">
+      <section className="lg:col-span-2 flex flex-col items-center justify-center py-6 lg:px-20 gap-6 h-screen">
+        <div className="overflow-y-auto w-full hide_scrollbar flex flex-col gap-5 pb-20 lg:pb-0">
           <ProfileForm />
           <SocialLinksForm />
           <AdditionalLinksForm />
 
           <Background />
 
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-2 justify-center items-center w-full '>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-2 justify-center items-center w-full ">
             <DemoData />
             <Publish />
             <ShortenerButton />
-            <Link
-              target='_blank'
-              href="https://github.com/sujjeee/linknode"
-              className={buttonVariants()}>
-              <Github className='mr-2 h-4 w-4' />
-              Github
-            </Link>
           </div>
         </div>
       </section>
 
-      <section className='hidden lg:flex justify-end items-center'>
+      <section className="hidden lg:flex justify-end items-center">
         <MobileMockup />
       </section>
 
-      <div className='lg:hidden'>
+      <div className="lg:hidden">
         <PreviewButton />
       </div>
-
     </main>
-  )
+  );
 }
